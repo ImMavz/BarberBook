429ddf2515a3c9cf5130e7f025caac7ae6706a81
Samuel Escobar Rivera <117455927+Samth18@users.noreply.github.com>
2025-10-02 23:00:10 -0500

    Reorder findOne method in users.controller.ts

--- barber-book-backend/src/users/users.controller.ts
+++ barber-book-backend/src/users/users.controller.ts
@@ -17,11 +17,6 @@ export class UsersController {
     return this.usersService.findAll();
     }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-    }
-
     @Patch(':id')
     update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
@@ -45,6 +40,11 @@ export class UsersController {
     @Get('dueños')
     findDuenos() {
         return this.usersService.findDuenos();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.usersService.findOne(+id);
     }
 
     @Get('estadisticas/roles') // GET /users/estadisticas/roles
