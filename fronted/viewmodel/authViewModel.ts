e5c714124c9f76a6372a60b1124f2800d5aecde8
ImMavz <josephherrera1407@gmail.com>
2025-12-01 23:01:33 -0500

    cambio de authviewmodel para que traiga al frontend el token o los datos de si es barbero.

--- fronted/viewmodel/authViewModel.ts
+++ fronted/viewmodel/authViewModel.ts
@@ -4,19 +4,13 @@ import { saveToken, saveUsuario } from "../utils/authStorage";
 
 export const useAuthViewModel = () => {
   const [loading, setLoading] = useState(false);
-  //const API_URL = "http://192.168.80.14:3000";
-  const API_URL = "http://192.168.1.32:3000" //API Juanito 
+  const API_URL = "http://192.168.80.14:3000";
 
-  // -----------------------
-  // REGISTRO
-  // -----------------------
   const register = async (user: any) => {
     try {
       setLoading(true);
       const res = await axios.post(`${API_URL}/users`, user);
-
       return { success: true, data: res.data };
-
     } catch (error: any) {
       return { success: false, error: error.response?.data };
     } finally {
@@ -24,9 +18,6 @@ export const useAuthViewModel = () => {
     }
   };
 
-  // -----------------------
-  // LOGIN
-  // -----------------------
   const login = async (identifier: string, password: string) => {
     try {
       setLoading(true);
@@ -36,15 +27,10 @@ export const useAuthViewModel = () => {
         contraseña: password,
       });
 
-      // 🔥 GUARDAR TOKEN Y USUARIO
       await saveToken(res.data.access_token);
       await saveUsuario(res.data.usuario);
 
-      console.log("🔐 TOKEN GUARDADO");
-      console.log("👤 USUARIO GUARDADO");
-
       return { success: true, data: res.data };
-
     } catch (error: any) {
       console.log("⚠ ERROR LOGIN:", error.response?.data || error.message);
       return { success: false, error: error.response?.data };
